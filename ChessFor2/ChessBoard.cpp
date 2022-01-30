f5d0815fad980034c797c76996c3b7fc06357ff6
Ruben Sanchez Castellano <tisi1988@gmail.com>
2022-01-30 02:40:50 +0100

    Create chessboard pieces

--- ChessFor2/ChessBoard.cpp
+++ ChessFor2/ChessBoard.cpp
@@ -1,3 +1,25 @@
 #include "ChessBoard.h"
 
-ChessBoard::ChessBoard() {}
+#include <tuple>
+#include <vector>
+
+ChessBoard::ChessBoard() { initializePieces(); }
+
+Tile ChessBoard::getTile(Position const &pos) const {
+  return Tile(m_tiles[pos.getX()][pos.getY()]);
+}
+
+void ChessBoard::initializePieces() {
+  static auto const pieceOrder = std::vector<PieceType>{
+      PieceType::TOWER, PieceType::HORSE,  PieceType::BISHOP, PieceType::QUEEN,
+      PieceType::KING,  PieceType::BISHOP, PieceType::HORSE,  PieceType::TOWER};
+
+  for (auto &row : {0, 1}) {
+    for (int col = 0; col < 8; col++) {
+      m_tiles[row][col] = Tile(Piece(
+          row == 0 ? pieceOrder[col] : PieceType::PAWN, PieceColor::BLACK));
+      m_tiles[7 - row][col] = Tile(Piece(
+          row == 0 ? pieceOrder[col] : PieceType::PAWN, PieceColor::WHITE));
+    }
+  }
+}
--- ChessFor2/ChessBoard.h
+++ ChessFor2/ChessBoard.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#include "Position.h"
+#include "Tile.h"
+
 class ChessBoard {
 public:
   ChessBoard();
+
+  Tile getTile(Position const &pos) const;
+
+private:
+  void initializePieces();
+
+  Tile m_tiles[8][8];
 };
