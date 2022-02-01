78aa15b2bb1e36787a32838f7c40b8ab5d811636
Ruben Sanchez Castellano <tisi1988@gmail.com>
2022-02-02 00:30:34 +0100

    Implement horse movement

--- ChessFor2/pieces/Horse.cpp
+++ ChessFor2/pieces/Horse.cpp
@@ -1,5 +1,7 @@
 #include "Horse.h"
 
+#include "../ChessBoard.h"
+
 Horse::Horse(PieceColor const &color) : Piece(color) {}
 
 Horse::~Horse() {}
@@ -8,6 +10,30 @@ PieceType Horse::getType() const { return PieceType::HORSE; }
 
 std::vector<Position> Horse::getMoves(ChessBoard *board,
                                       Position const &origin) const {
-  // TODO
-  return {};
+  // Horses move in an 'L' shape
+  // This list contains the relative position movement
+  static const std::vector<std::pair<int, int>> MOVES_DELTAS = {
+      {-2, -1}, {-2, +1}, {-1, +2}, {+1, +2},
+      {+2, +1}, {+2, -1}, {+1, -2}, {-1, -2}};
+
+  std::vector<Position> moves;
+
+  for (auto &&[rowDelta, colDelta] : MOVES_DELTAS) {
+    Position const p =
+        Position(origin.getX() + rowDelta, origin.getY() + colDelta);
+
+    if (!board->isValidPosition(p)) {
+      continue;
+    }
+
+    // Discard if there's a piece of same color
+    if (!board->getTile(p)->isEmpty() &&
+        board->getTile(p)->getPiece()->getColor() == m_color) {
+      continue;
+    }
+
+    moves.push_back(p);
+  }
+
+  return moves;
 }
