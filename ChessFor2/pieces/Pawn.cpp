c18c38b84bd0e4985c4f1573deb689e120ad005c
Ruben Sanchez Castellano <tisi1988@gmail.com>
2022-02-01 23:39:41 +0100

    Completed Pawn movement computation

--- ChessFor2/pieces/Pawn.cpp
+++ ChessFor2/pieces/Pawn.cpp
@@ -18,12 +18,69 @@ PieceType Pawn::getType() const { return PieceType::PAWN; }
 std::vector<Position> Pawn::getMoves(ChessBoard *board,
                                      Position const &origin) const {
   std::vector<Position> dest;
-  dest.push_back(Position(origin.getX() + 1 * m_direction, origin.getY()));
 
-  // Pawns can move 2 tiles if their in the initial tile
-  if (isInInitialTile(origin.getX(), m_color)) {
-    dest.push_back(Position(origin.getX() + 2 * m_direction, origin.getY()));
-  }
+  addFrontMoves(dest, board, origin);
+  addKills(dest, board, origin);
 
   return dest;
+}
+
+void Pawn::addFrontMoves(std::vector<Position> &moves, ChessBoard *board,
+                         Position const &origin) const {
+  try {
+    Position const frontTile =
+        Position(origin.getX() + 1 * m_direction, origin.getY());
+
+    // If there's no piece on that position, it can move there
+    if (board->isValid(frontTile) && !board->getTile(frontTile)->getPiece()) {
+      moves.push_back(frontTile);
+
+      // Pawns can move an extra tile forward if their in the initial tile
+      Position const secondTile =
+          Position(frontTile.getX() + 1 * m_direction, frontTile.getY());
+      if (isInInitialTile(origin.getX(), m_color) &&
+          board->isValid(secondTile) &&
+          !board->getTile(secondTile)->getPiece()) {
+        moves.push_back(secondTile);
+      }
+    }
+  } catch (std::runtime_error const &e) {
+    throw std::runtime_error("Error trying to check front moves for Pawn at " +
+                             origin.toString() + ": " + e.what());
+  }
+}
+
+void Pawn::addKills(std::vector<Position> &moves, ChessBoard *board,
+                    Position const &origin) const {
+
+  try {
+    // It also can move diagonally front-left if there's an enemy piece there
+    Position const fl =
+        Position(origin.getX() + 1 * m_direction, origin.getY() - 1);
+
+    if (checkPossibleKill(board, fl)) {
+      moves.push_back(fl);
+    }
+
+    // It also can move diagonally front-right if there's an enemy piece there
+    Position const fr =
+        Position(origin.getX() + 1 * m_direction, origin.getY() + 1);
+
+    if (checkPossibleKill(board, fr)) {
+      moves.push_back(fr);
+    }
+  } catch (std::runtime_error const &) {
+    // We end up in an invalid position
+    // either in the left or the right forward
+  }
+}
+
+bool Pawn::checkPossibleKill(ChessBoard *board, Position const &pos) const {
+  Tile *tile{nullptr};
+
+  if (board->isValid(pos)) {
+    tile = board->getTile(pos);
+  }
+
+  return tile && !tile->isEmpty() && tile->getPiece()->getColor() != m_color;
 }
--- ChessFor2/pieces/Pawn.h
+++ ChessFor2/pieces/Pawn.h
@@ -11,4 +11,27 @@ public:
   PieceType getType() const override;
   std::vector<Position> getMoves(ChessBoard *board,
                                  Position const &origin) const override;
+
+private:
+  /**
+   * @brief Checks if the Pawn can move forward either one or two tiles.
+   * @param moves The list of possible movements forward.
+   * @param board The chess board
+   * @param origin The Pawn's original position.
+   */
+  void addFrontMoves(std::vector<Position> &moves, ChessBoard *board,
+                     Position const &origin) const;
+
+  /**
+   * @brief Checks if the Pawn can kill an enemy piece located at the front-left
+   * or front-right of the Pawn's current position.
+   * @param moves The list of possible movements forward.
+   * @param board The chess board
+   * @param origin The Pawn's original position.
+   */
+  void addKills(std::vector<Position> &moves, ChessBoard *board,
+                Position const &origin) const;
+
+  bool checkPossibleKill(ChessBoard *board,
+                         Position const &pos) const;
 };
